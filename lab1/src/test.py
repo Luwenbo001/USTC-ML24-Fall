79daf350e03a7872fef894c9b46378bab800d542
Luwenbo001 <77435866+Luwenbo001@users.noreply.github.com>
2024-11-11 07:16:16 +0000

    Auto fix formatting from GitHub Actions run 1

--- lab1/src/test.py
+++ lab1/src/test.py
@@ -1,6 +1,6 @@
 def data_preprocessing_regression(data_path: str, saved_to_disk: bool = False) -> Dataset:
     r"""Load and preprocess the training data for the regression task.
-    
+
     Args:
         data_path (str): The path to the training data.If you are using a dataset saved with save_to_disk(), you can use load_from_disk() to load the dataset.
 
@@ -12,11 +12,11 @@ def data_preprocessing_regression(data_path: str, saved_to_disk: bool = False) -
     if saved_to_disk:
         dataset = load_from_disk(data_path)
     else:
-        dataset = load_dataset(data_path) 
+        dataset = load_dataset(data_path)
     # Preprocess the dataset
     # Use dataset.to_pandas() to convert the dataset to a pandas DataFrame if you are more comfortable with pandas
     # TODO：You must do something in 'Run_time' column, and you can also do other preprocessing steps
-    dataset['train'] = dataset['train'].remove_columns('__index_level_0__')
+    dataset["train"] = dataset["train"].remove_columns("__index_level_0__")
     # data = dataset['train'].to_pandas()
 
     # Standardize each column in the dataset
@@ -30,4 +30,4 @@ def data_preprocessing_regression(data_path: str, saved_to_disk: bool = False) -
     # output = "./output.txt"
     # with open(output,'w') as f:
     #     print(data, file=f)
-    return dataset['train']
+    return dataset["train"]
